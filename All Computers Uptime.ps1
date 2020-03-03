cd76dc626e4c704136a2d8f02d0eb765fbba1013
Proph3tt <52930971+Proph3tt@users.noreply.github.com>
2020-03-03 10:58:13 -0800

    Update All Computers Uptime.ps1

--- All Computers Uptime.ps1
+++ All Computers Uptime.ps1
@@ -1,6 +1,6 @@
 ﻿######################################################################################################################################
-# created by Elias 3-3-20
-# last modified by Elias on 3-3-20                                                     
+# created by Proph3tt 3-3-20
+# last modified by Proph3tt on 3-3-20                                                     
 #
 # This script gets the time every computer was last restarted.
 # To do this locally, you can use the following code and run it as a script:
@@ -14,10 +14,6 @@
 # }
 #
 #
-# Why would you want this you say? Because people will lie about rebooting and blame us for things not working.
-# For example: drives missing, slow computer, no updates going through, etc.
-# Also, we keep telling people to reboot and they are not doing it, so this can be used to show statistics/numbers.
-# For the users that do not reboot, we can force one via group policy.
 # Working to improve this script by running it automatically every month, checking what users have not rebooted in 30+ days, then forcing a reboot on those users
 ######################################################################################################################################
 
@@ -33,7 +29,7 @@ Import-Module ActiveDirectory
 #Look in for the LastBoottimeValue in the csname of said computer, then convert the date into something we can easily read.
 #make a variable called results so that you can export all of the results to a CSV file. Otherwise, script does not know what to export.
 ForEach ($computer in (Get-ADComputer -Filter *)) {
-Get-WmiObject Win32_OperatingSystem -ComputerName $computer.Name | select csname, @{LABEL='LastBootUpTime';EXPRESSION={$_.ConverttoDateTime($_.lastbootuptime)}} | Export-Csv C:\Users\elmartinez\documents\UptimeReport.csv -NoTypeInformation -Encoding UTF8 -Append
+Get-WmiObject Win32_OperatingSystem -ComputerName $computer.Name | select csname, @{LABEL='LastBootUpTime';EXPRESSION={$_.ConverttoDateTime($_.lastbootuptime)}} | Export-Csv C:\Users\users\documents\UptimeReport.csv -NoTypeInformation -Encoding UTF8 -Append
 }
 
 
